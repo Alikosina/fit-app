83f881fba399e399d17d0ef7964f3bf1edda107f
Ali Sloyan <alisloyan@MacBook-Pro-Ali.local>
2019-07-31 21:24:51 +0300

    Added tsm script

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as styles from "./App.module.scss";
 
 const AppContainer = () => (
-  <div className={styles.App}>
+  <div className={styles.app}>
     <h1>Hello Fit App!</h1>
   </div>
 );
