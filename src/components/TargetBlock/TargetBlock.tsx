d48511b5b45ccdecdc75f1f13d8c201426522cc1
Ali Sloyan <alisloyan@MacBook-Pro-Ali.local>
2019-07-31 22:42:30 +0300

    Prepared images loader

--- src/components/TargetBlock/TargetBlock.tsx
+++ src/components/TargetBlock/TargetBlock.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 import * as styles from "./TargetBlock.module.scss";
-import { TargetBlockModel } from './models/TargetBlockModel'
+import { TargetBlockModel } from "./models/TargetBlockModel";
 
-const TargetBlock = (props:TargetBlockModel.Props) =>(
-    <div className={styles.target}> BLOCK <span className={styles.targetItem}>MyText</span>
-          <div>
-            {props.image}
-          </div>
-          <div>
-            {props.description}
-          </div>
-        </div>
-)
+const TargetBlock = (props: TargetBlockModel.Props) => (
+  <div className={styles.target}>
+    {" "}
+    BLOCK <span className={styles.targetItem}>MyText</span>
+    <div>
+      <img src={props.image} alt="" />
+    </div>
+    <div>{props.description}</div>
+  </div>
+);
 
 export default TargetBlock;
--- webpack.config.js
+++ webpack.config.js
@@ -50,30 +50,26 @@ module.exports = {
           "sass-loader"
         ]
       },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "fonts/"
-            }
-          }
-        ]
-      },
+      // {
+      //   test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      //   use: [
+      //     {
+      //       loader: "file-loader",
+      //       options: {
+      //         name: "[name].[ext]",
+      //         outputPath: "fonts/"
+      //       }
+      //     }
+      //   ]
+      // },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true,
-              disable: true
-            }
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[ext]"
           }
-        ]
+        }
       }
     ]
   },
